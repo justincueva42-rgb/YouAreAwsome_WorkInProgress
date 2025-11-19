75511684592a5d9bdecc627084792e7e8ae9dd70
justincueva42-rgb <justincueva42@gmail.com>
2025-11-19 11:35:47 -0800

    Changed so that it now has repeat and while loops as well as added arrays

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -10,41 +10,62 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
+    @State private var lastImageNumber = -1
+    @State private var lastMessageNumber = -1 // lastMessageNumber will never be -1
     var body: some View {
         VStack {
             
-            Spacer()
+            Text(message)
+            .font(.largeTitle)
+            .fontWeight(.heavy)
+            .foregroundStyle(.red)
+            .multilineTextAlignment(.center)
+            .minimumScaleFactor(0.5)
+            .frame(height: 120)
+            .animation(.easeInOut(duration: 0.15), value: message)
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
+                .animation(.default, value: imageName)
             
-                Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
             Spacer()
             
                 
                 Button("Show Message!") {
-                    let message1 = "You are Awesome!"
-                    let message2 = "You are Great!"
+                    let messages = ["You Are Awesome!", "When the Genius Bar Needs Help, They Call You!","You Are Great!", "You Are Fantastic", "Fabulous? That's You!","You Make Me Smile!"]
+
+                    //generate a ranodm number messageNumber to use as an Index
+                    //if messageNumber == lastMessageNumber
+                    //keep gneerating a new random messageNumber
+                    // until you get a messageNumber != mlastMessageNumber
+                    //set messageString to messages(messageNumber)
+                    // update the lastMessageNumber with messageNumber
                     
-                    message = (message == message1 ? message2 : message1)
+                    var messageNumber = Int.random(in: 0...messages.count-1)
+                    while messageNumber == lastMessageNumber{
+                        messageNumber = Int.random(in: 0...messages.count-1)
+                    }
+                    message = messages [messageNumber]
+                    lastImageNumber = messageNumber
+                    
+                    message = messages[Int.random(in: 0...messages.count-1)]
+                   
+                    var imageNumber : Int
+                    repeat{
+                        imageNumber = Int.random(in:0...9)
+                    }while imageNumber == lastImageNumber
                     imageName = "image\(imageNumber)"
-                    imageNumber += 1
-                    if imageNumber > 9{
-                                            imageNumber = 0
-                                        }
-                    
+                    lastImageNumber = imageNumber
                 }
             .buttonStyle(.borderedProminent)
             .font(.title2)
             .tint(.blue)
+            
         }
+        
         
         .padding()
         
